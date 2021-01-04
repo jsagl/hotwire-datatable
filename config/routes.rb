36aab66c7fd2c0f05f05df23e73e4bf1ba34c265
Jean Saglio <jean.saglio@karhoo.com>
2021-01-04 17:50:50 +0100

    added root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
+  root to: 'books#index'
+
   resources :books, only: [:index, :show, :edit, :update]
 end
